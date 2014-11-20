b695b6f1c03b691a4102acb995923a97165506d4
Low Kian Seong <kianseong@gmail.com>
2014-11-20 10:36:49 +0800

    Since circle is free now ... just going to enable it :)

--- install-elixir.sh
+++ install-elixir.sh
@@ -5,5 +5,5 @@ export PATH=`pwd`/otp_src_17.0/bin:$PATH
 export STABLE_ELIXIR_VERSION="1.0.2"
 
 if [ ! -e elixir/bin/elixir ]; then
-   wget --no-clobber -q https://github.com/elixir-lang/elixir/archive/v${STABLE_ELIXIR_VERSION}.zip && unzip -qq precompiled.zip -d elixir
+   wget --no-clobber -q https://github.com/elixir-lang/elixir/releases/download/v${STABLE_ELIXIR_VERSION}/Precompiled.zip && unzip -qq precompiled.zip -d elixir
 fi
