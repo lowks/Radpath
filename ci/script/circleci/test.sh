448aeeb4ff55808bca8c6f594bef92886b0b2c61
lowks <kianseong@gmail.com>
2014-12-14 09:33:56 +0800

    Make circleci report to inchci too. Hmm tougher that I thought.

--- ci/script/circleci/test.sh
+++ ci/script/circleci/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-
 export MIX_ENV="test"
 export PATH="$HOME/dependencies/erlang/bin:$HOME/dependencies/elixir/bin:$PATH"
-
 mix amrita --trace
+mix deps.get --only docs
+mix inch.report
