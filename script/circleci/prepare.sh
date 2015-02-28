99ed5a4acfd44b5d0d56599738857e71d41fc21b
lowks <kianseong@gmail.com>
2015-03-01 01:34:53 +0800

    Guess what?! It's upgrade time for Elixir

--- script/circleci/prepare.sh
+++ script/circleci/prepare.sh
@@ -3,7 +3,7 @@
 set -e
 
 export ERLANG_VERSION="17.1"
-export ELIXIR_VERSION="v1.0.2"
+export ELIXIR_VERSION="v1.0.3"
 export INSTALL_PATH="$HOME/dependencies"
 
 export ERLANG_PATH="$INSTALL_PATH/otp_src_$ERLANG_VERSION"
