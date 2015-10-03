5186b7f7ed0c2d69fba8cb8aa0314639e08fd55e
lowks <kianseong@gmail.com>
2015-10-03 17:05:00 +0800

    Upgrade circleci elixir to 1.1.1

--- script/circleci/prepare.sh
+++ script/circleci/prepare.sh
@@ -3,7 +3,7 @@
 set -e
 
 export ERLANG_VERSION="17.5"
-export ELIXIR_VERSION="v1.0.4"
+export ELIXIR_VERSION="v1.1.1"
 export INSTALL_PATH="$HOME/dependencies"
 
 export ERLANG_PATH="$INSTALL_PATH/otp_src_$ERLANG_VERSION"
